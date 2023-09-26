a33c334a90ac448317eebd8b8b0252ba6661cbb7
Raúl Montón Pinillos <raul_m_p@me.com>
2023-09-26 19:41:32 +0200

    feat: Added GCD for multicore + updated README.md

--- HPC_Tools_1/dgesv.c
+++ HPC_Tools_1/dgesv.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#if defined(__APPLE__)
+    #define USE_APPLE_DISPATCH
+    #include <dispatch/dispatch.h>
+#endif
 
 #include "dgesv.h"
 
@@ -17,21 +21,27 @@ void swap(double *pa, double *pb) {
 
 int my_dgesv(int n, double *a, double *b) {
     
-    int i, icol, irow, j, k, l, ll;
-    double big, dum, pivinv;
     int *indxc = generate_int_vector(n);
     int *indxr = generate_int_vector(n);
     int *ipiv = generate_int_vector(n);
     
-    for(j=0; j<n; j++) {
+    double big, dum, pivinv;
+    int icol, irow;
+        
+    #pragma clang loop vectorize(enable) interleave(enable)
+    for(int j=0; j<n; j++) {
         ipiv[j] = 0;
     }
     
-    for(i=0; i<n; i++) {
+    #if defined(USE_APPLE_DISPATCH)
+    dispatch_queue_t concurrentQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
+    #endif
+    
+    for(int i=0; i<n; i++) {
         big = 0.0;
-        for (j=0; j<n; j++) {
+        for (int j=0; j<n; j++) {
             if (ipiv[j] != 1) {
-                for(k=0; k<n; k++) {
+                for(int k=0; k<n; k++) {
                     if (ipiv[k] == 0) {
                         if (fabs(a[j*n + k]) >= big) {
                             big = fabs(a[j*n + k]);
@@ -44,7 +54,7 @@ int my_dgesv(int n, double *a, double *b) {
         }
         ++(ipiv[icol]);
         if (irow != icol) {
-            for(l=0; l<n; l++) {
+            for(int l=0; l<n; l++) {
                 swap(&a[irow*n + l], &a[icol*n + l]);
                 swap(&b[irow*n + l], &b[icol*n + l]);
             }
@@ -57,29 +67,43 @@ int my_dgesv(int n, double *a, double *b) {
         }
         pivinv = 1.0 / a[icol*n + icol];
         a[icol*n + icol] = 1.0;
-        for(l=0; l<n; l++) {
+        
+        #pragma clang loop vectorize(enable) interleave(enable)
+        for(int l=0; l<n; l++) {
             a[icol*n + l] *= pivinv;
             b[icol*n + l] *= pivinv;
         }
-        for(ll=0; ll<n; ll++) {
+        
+        // On Apple platforms, execute concurrently in all available cores
+        // using Apple's dispatch library.
+        #if defined(USE_APPLE_DISPATCH)
+        dispatch_apply(n, concurrentQueue, ^(size_t ll) {
+        #else
+        for(int ll=0; ll<n; ll++) {
+        #endif
             if (ll != icol) {
-                dum = a[ll*n + icol];
+                double dum = a[ll*n + icol];
                 a[ll*n + icol] = 0.0;
-                for (l=0; l<n; l++) {
+                #pragma clang loop vectorize(enable) interleave(enable)
+                for (int l=0; l<n; l++) {
                     a[ll*n + l] -= a[icol*n + l] * dum;
                     b[ll*n + l] -= b[icol*n + l] * dum;
                 }
             }
+        #if defined(USE_APPLE_DISPATCH)
+        });
+        #else
         }
+        #endif
     }
-    
-    for(l=n-1; l>=0; l--) {
+
+    for(int l=n-1; l>=0; l--) {
         if (indxr[l] != indxc[l]) {
-            for(k=0; k<n; k++) {
+            for(int k=0; k<n; k++) {
                 swap(&a[k*n + indxr[l]], &a[k*n + indxc[l]]);
             }
         }
     }
-    
+        
     return 0;
 }
---
+++ README.md
@@ -0,0 +1,18 @@
+# Gauss-Jordan Elimination
+This repo implements Gauss-Jordan elimination in C. Loosely based in _Numerical Recipes in C_, Chapter 2.1.
+
+## Observations
+On a M1 Pro (8P cores + 2E cores), for a 2048x2048 matrix, the LAPACK version executes in ~180ms while this version takes ~4700ms. 
+
+The reason for this performance difference is obvious after looking at a performance trace of both the LAPACK version and the custom one:
+- LAPACK uses a single CPU core for a brief period, and then launches a 10 thread computation. This is obviously because LAPACK's `dgsev` is not based on Gauss-Jordan, but rather on LU Decomposition. So the decomposition is computed first (on a single thread), and then a solver is run for every row of matrix B (concurrently, on all threads).
+- Our version, based on the Gauss-Jordan elimination, can't be multithreaded (mostly, there's a small loop section that could be made multicore, shaving off ~25% of the execution time).
+
+Not only is our version slower, it's also less numerically accurate (since it's performing a lot of sequential operations, slowly adding precision errors over time). Because of this, `check_result` may return `Result is wrong!` unless a bigger `epsilon` is chosen (`1e-3` seems to work fine for 4096x406 matrices).
+
+## Tools
+Compiled using LLVM + Clang 14.0.3 (Apple's version, bundled with Xcode). Because of that, code is annotated with `#pragma clang loop...` instead of `#pragma omp simd...` (Apple's Clang does not have OpenMP support). Not that it matters: decompiling the binary shows that the code uses ARM's vector extensions even if you don't include the compiler hints.
+
+If an Apple platform is detected (`#if defined(__APPLE__)` for C source files, `ifeq ($(OS), Darwin)` for the makefile), the code will use Apple's Grand Central Dispatch (`<dispatch/dispatch.h>`) for a slight multicore boost in one of the loops.
+
+The repo contains a simple Xcode project configured with an external build system preset in order to ensure the makefile is used for compilation.
