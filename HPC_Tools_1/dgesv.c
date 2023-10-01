4b1774018049d279848a5a78fc13d202d9037c46
Raúl Montón Pinillos <raul_m_p@me.com>
2023-10-01 17:02:40 +0200

    feat: Gaussian elimination + backsubstitution

--- HPC_Tools_1/dgesv.c
+++ HPC_Tools_1/dgesv.c
@@ -13,100 +13,141 @@ int *generate_int_vector(unsigned int length) {
   return vector;
 }
 
+double *generate_double_vector(unsigned int length) {
+  double *vector = (double *) malloc(sizeof(double) * length);
+  return vector;
+}
+
 void swap(double *pa, double *pb) {
     double temp = *pa;
     *pa = *pb;
     *pb = temp;
 }
 
+void print_matrix(int n, double *a) {
+    for (int i=0; i<n; i++) {
+        for(int j=0; j<n; j++){
+             printf("%lf     ", a[i*n + j]);
+        }
+        printf("\n");
+    }
+}
+
 int my_dgesv(int n, double *a, double *b) {
     
-    int *ipiv = generate_int_vector(n);
+    double *vv = generate_double_vector(n);
     
-    double big, dum, pivinv;
+    double big, dum, temp;
     int imax;
-        
-    #pragma clang loop vectorize(enable) interleave(enable)
-    for(int j=0; j<n; j++) {
-        ipiv[j] = 0;
-    }
     
     #if defined(USE_APPLE_DISPATCH)
-    dispatch_queue_t concurrentQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
+    dispatch_queue_t concurrent_queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
     int dispatch_stride = 64;
     #endif
     
-    for(int i=0; i<n; i++) {
+    for (int i = 0; i<n; i++) {
         big = 0.0;
-        for (int j=0; j<n; j++) {
-            if (ipiv[j] == 0) {
-                // Search for pivot element only below the current row
-                if (fabs(a[j*n + j]) >= big) {
-                    big = fabs(a[j*n + j]);
-                    imax = j;
-                }
+        for (int j = 0; j<n; j++) {
+            if ((temp = fabs(a[i*n + j])) > big) {
+                big = temp;
             }
         }
-        ++(ipiv[imax]);
-                
-        // Check whether rows need to be swapped
-        if (i != imax) {
-            // Swap rows...
-            for(int l=0; l<n; l++) {
-                swap(&a[imax*n + l], &a[imax*n + l]);
-                swap(&b[imax*n + l], &b[imax*n + l]);
+        vv[i] = 1.0 / big;
+    }
+
+    for(int k=0; k<n; k++) {
+        big = 0.0;
+        // Search for pivot element only at or below the current row.
+        for(int i=k; i<n; i++) {
+            double temp = vv[i] * fabs(a[i*n + k]);
+            if (temp > big) {
+                big = temp;
+                imax = i;
             }
+        }
+                
+        // Check whether rows need to be swapped.
+        if (k != imax) {
+            // Swap rows...
+            for(int j=0; j<n; j++) {
+                swap(&a[imax*n + j], &a[k*n + j]);
+                swap(&b[imax*n + j], &b[k*n + j]);
+            }
+            vv[imax] = vv[k];
         }
 
-        if (a[imax*n + imax] == 0.0) {
+        // If the pivot element is zero, either the matrix is singular or we fucked up.
+        if (a[k*n + k] == 0.0) {
             printf("Singular matrix");
             exit(0);
         }
-        pivinv = 1.0 / a[imax*n + imax];
-        a[imax*n + imax] = 1.0;
         
-        #pragma clang loop vectorize(enable) interleave(enable)
-        for(int l=0; l<n; l++) {
-            a[imax*n + l] *= pivinv;
-            b[imax*n + l] *= pivinv;
-        }
-                
-        // On Apple platforms, execute concurrently in all available cores
-        // using Apple's dispatch library.
+        // MARK: - Elimination
+        // On Apple platforms, execute concurrently in all available cores using Apple's
+        // dispatch library.
         #if defined(USE_APPLE_DISPATCH)
-        dispatch_apply(n / dispatch_stride, concurrentQueue, ^(size_t idx) {
-            int max = (idx + 1) * dispatch_stride;
+        int number_of_threadgroups = ((n - (k+1)) / dispatch_stride) + 1;
+        dispatch_apply(number_of_threadgroups, concurrent_queue, ^(size_t idx) {
+            int max = k + 1 + (idx + 1) * dispatch_stride;
             if (max > n) {
                 max = n;
             }
-            for(int ll = idx * dispatch_stride; ll<max; ll++) {
-                if (ll != imax) {
-                    double dum = a[ll*n + imax];
-                    a[ll*n + imax] = 0.0;
-                    #pragma clang loop vectorize(enable) interleave(enable)
-                    for (int l=0; l<n; l++) {
-                        a[ll*n + l] -= a[imax*n + l] * dum;
-                        b[ll*n + l] -= b[imax*n + l] * dum;
-                    }
+            for (int i = k + 1 + (idx * dispatch_stride); i<max; i++) {
+                double temp = a[i*n + k] / a[k*n + k];
+                #pragma clang loop vectorize(enable) interleave(enable)
+                for (int j = 0; j<n; j++) {
+                    a[i*n + j] -= temp * a[k*n + j];
+                    b[i*n + j] -= temp * b[k*n + j];
                 }
             }
         });
         // On non-Apple platforms, use OpenMP to parallelize the loop instead.
         #else
         #pragma omp parallel for
-        for(int ll=0; ll<n; ll++) {
-            if (ll != imax) {
-                double dum = a[ll*n + imax];
-                a[ll*n + imax] = 0.0;
-                #pragma clang loop vectorize(enable) interleave(enable)
-                for (int l=0; l<n; l++) {
-                    a[ll*n + l] -= a[imax*n + l] * dum;
-                    b[ll*n + l] -= b[imax*n + l] * dum;
-                }
+        for (int i = k+1; i<n; i++) {
+            temp = a[i*n + k] / a[k*n + k];
+            for (int j = 0; j<n; j++) {
+                a[i*n + j] -= temp * a[k*n + j];
+                b[i*n + j] -= temp * b[k*n + j];
             }
         }
         #endif
     }
+    
+    // MARK: - Backsubstitution
+    // On Apple platforms, execute concurrently in all available cores using Apple's
+    // dispatch library.
+    #if defined(USE_APPLE_DISPATCH)
+    dispatch_apply((n / dispatch_stride) + 1, concurrent_queue, ^(size_t idx) {
+        int max = (idx + 1) * dispatch_stride;
+        if (max > n) {
+            max = n;
+        }
+        for (int k = idx * dispatch_stride; k < max; k++) {
+            for (int i=n-1; i>=0; i--) {
+                double sum = b[i*n + k];
+                #pragma clang loop vectorize(enable) interleave(enable)
+                for(int j=i+1; j<n; j++) {
+                    sum -= a[i*n + j] * b[j*n + k];
+                }
+                b[i*n + k] = sum / a[i*n + i];
+            }
+        }
+    });
+    // On non-Apple platforms, use OpenMP to parallelize the loop instead.
+    #else
+    #pragma omp parallel for
+    // On non-Apple platforms, use OpenMP to parallelize the loop instead.
+    for (int k=0; k<n; k++) {
+        for (int i=n-1; i>=0; i--) {
+            double sum = b[i*n + k];
+            for(int j=i+1; j<n; j++) {
+                sum -= a[i*n + j] * b[j*n + k];
+            }
+            b[i*n + k] = sum / a[i*n + i];
+        }
+    }
+    #endif
         
     return 0;
 }
